7e973995bc4ce14eeb9396d419dc7ce6a4b1acf2
manhnh251995 <41736966+manhnh251995@users.noreply.github.com>
2021-03-03 12:06:31 +0700

    Update checkoutgit.groovy

--- vars/checkoutgit.groovy
+++ vars/checkoutgit.groovy
@@ -1,6 +1,7 @@
 def call(URL){
          
         def url = "git clone " + URL;
+        println url
         def proc = url.execute();
         def outputStream = new StringBuffer();
         proc.waitForProcessOutput(outputStream, System.err)
