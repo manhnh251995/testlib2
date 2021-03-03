58d74087af5326b8bdd1457dbc04feae9c5225a8
manhnh251995 <41736966+manhnh251995@users.noreply.github.com>
2021-03-03 17:18:36 +0700

    Update checkoutgit.groovy

--- vars/checkoutgit.groovy
+++ vars/checkoutgit.groovy
@@ -1,21 +1,9 @@
-def call() {
-
-    checkout scm
-
-    def scmVars = [:]
-
-    
-    dir('testlib') {
-
-        def repoScmVars = checkout([
-            $class: 'GitSCM',
-            branches: [[name: "main"]], 
-            browser: [$class: 'GithubWeb', repoUrl: 'https://github.com/manhnh251995/node-hello.git'],
-           // extensions: [
-            //    [$class: 'CleanBeforeCheckout'],
-           // ]     
-        ])
-        //scmVars.put('testlib', repoScmVars)
-    }
+def static executeCurlCommand(URL){
+         
+        def url = "curl " + URL;
+        def proc = url.execute();
+        def outputStream = new StringBuffer();
+        proc.waitForProcessOutput(outputStream, System.err)
+        return outputStream.toString();
 }
 
